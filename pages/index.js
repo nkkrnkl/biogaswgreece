ac3b1fce23bbde3fee838f51a91b83f026307878
Niki Karanikola <70606645+nkkrnkl@users.noreply.github.com>
2023-01-02 14:22:45 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -12,9 +12,6 @@ export default function Home() {
 
       <main>
         <Header title="Βιοαέριο Δυτικής Ελλάδας" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
       </main>
 
       <Footer />
